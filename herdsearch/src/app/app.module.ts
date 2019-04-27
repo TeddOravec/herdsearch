b3620d882b40671288aa20c923ec726ee9204724
Ryan Schirado <50056044+ryan-schirado@users.noreply.github.com>
2019-04-27 14:58:23 -0500

    angular no longer angry

--- herdsearch/src/app/app.module.ts
+++ herdsearch/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {AngularFireModule} from '@angular/fire';
-import {AngularFirestoreModule} from '@angular/fire/firestore';
-import {AngularFireStorageModule} from '@angular/fire/storage';
-import {environment} from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
--- herdsearch/src/environments/environment.ts
+++ herdsearch/src/environments/environment.ts
@@ -3,7 +3,26 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+<<<<<<< HEAD
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyApjl6xlDQZlb9EWg3Mkyrt0G4A60rEvDY",
+    authDomain: "herd-search.firebaseapp.com",
+    databaseURL: "https://herd-search.firebaseio.com",
+    projectId: "herd-search",
+    storageBucket: "herd-search.appspot.com",
+    messagingSenderId: "352023866897"
+=======
+  production: true,
+  firebase: {
+    apiKey: "...",
+    authDomain: "project.firebaseapp.com",
+    databaseURL: "https://project.firebaseio.com",
+    projectId: "project",
+    storageBucket: "project.appspot.com",
+    messagingSenderId: "..."
+>>>>>>> c41064f1b19561c63b31cc435a676bfd68cb7832
+  }
 };
 
 /*
