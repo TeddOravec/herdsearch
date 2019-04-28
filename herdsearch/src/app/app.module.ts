67c76aff292403c8595c23c1788bb0b5ca0f514b
vaughan-johnson <50056187+vaughan-johnson@users.noreply.github.com>
2019-04-28 14:09:15 -0500

    Add Profile Started

--- herdsearch/src/app/app.module.ts
+++ herdsearch/src/app/app.module.ts
@@ -8,20 +8,24 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { environment } from '../environments/environment';
+import { AddProfileComponent } from './add-profile/add-profile.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    FormsModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    AngularFireStorageModule,
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      AddProfileComponent
+   ],
+   imports: [
+      FormsModule,
+      AngularFireModule.initializeApp(environment.firebase),
+      AngularFirestoreModule,
+      AngularFireStorageModule,
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
